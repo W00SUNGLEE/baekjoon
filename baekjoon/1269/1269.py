c693c344965e3fe36fe4c5fe57f548f713de1297
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-06-21 12:01:27 +0900

    2022/06/21 1269

---
+++ baekjoon/1269/1269.py
@@ -0,0 +1,10 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+A = set(map(int, sys.stdin.readline().split()))
+B = set(map(int, sys.stdin.readline().split()))
+
+print(len(A-B)+len(B-A))
+
+print((A-B)&(B-A))
