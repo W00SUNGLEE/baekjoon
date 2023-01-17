5d212e41a2e685e1e7f2ff2cd0db3540b6a24734
W00SUNGLEE <vhxmfltm135@gmail.com>
2023-01-17 16:52:50 +0900

    2023/01/17 15666

---
+++ baekjoon/15666/15666.py
@@ -0,0 +1,13 @@
+import sys
+from itertools import combinations_with_replacement
+
+n, m = map(int, sys.stdin.readline().split())
+
+number_list = list(map(int, sys.stdin.readline().split()))
+
+number_list.sort()
+
+for sequence in sorted(set(combinations_with_replacement(number_list, m))):
+    for number in sequence:
+        print(number, end=' ')
+    print()
