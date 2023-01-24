7b611bbea5747632b3dff74e60ca98f38b37c513
W00SUNGLEE <vhxmfltm135@gmail.com>
2023-01-24 14:45:15 +0900

    2023/01/24 9251

---
+++ baekjoon/9251/9251.py
@@ -0,0 +1,15 @@
+import sys
+
+string1 = list(sys.stdin.readline().strip())
+string2 = list(sys.stdin.readline().strip())
+
+dp = [[0 for _ in range(len(string2)+1)] for _ in range(len(string1)+1)]
+
+for i in range(1, len(string1)+1):
+    for j in range(1, len(string2)+1):
+        if string1[i-1] == string2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+
+print(dp[-1][-1])
