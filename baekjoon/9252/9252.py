8ca05cd2d427963d74e12f315b671c58dd4d5e27
W00SUNGLEE <vhxmfltm135@gmail.com>
2023-03-09 19:04:55 +0900

    2023/03/09 9252

---
+++ baekjoon/9252/9252.py
@@ -0,0 +1,20 @@
+import sys
+
+string1 = list(sys.stdin.readline().strip())
+string2 = list(sys.stdin.readline().strip())
+
+dp = [["" for _ in range(len(string2) + 1)] for _ in range(len(string1) + 1)]
+
+for i in range(1, len(string1) + 1):
+    for j in range(1, len(string2) + 1):
+        if string1[i - 1] == string2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + string1[i - 1]
+
+        else:
+            if len(dp[i - 1][j]) > len(dp[i][j - 1]):
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i][j - 1]
+
+print(len(dp[-1][-1]))
+print(dp[-1][-1])
